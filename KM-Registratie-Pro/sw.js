248f1b2b1e990671c7d2a3673ae3c97d8de95cb0
LuigiA1964 <167955086+LuigiA1964@users.noreply.github.com>
2026-01-27 08:17:30 +0100

    Delete KM-Registratie-Pro directory

--- KM-Registratie-Pro/sw.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const CACHE_NAME='km-registratie-pwa-v1.0.0';
-const STATIC_ASSETS=['./','/index.html','./manifest.json','./icons/icon-192x192.png','./icons/icon-512x512.png','https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(STATIC_ASSETS)).then(()=>self.skipWaiting()))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(n=>Promise.all(n.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html'))))});
