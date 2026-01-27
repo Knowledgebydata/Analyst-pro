4b3d70059d368e8d71c39a5ac689e52ccdc17527
LuigiA1964 <167955086+LuigiA1964@users.noreply.github.com>
2026-01-27 08:22:10 +0100

    Add files via upload

---
+++ ap-straat-pwa/sw.js
@@ -0,0 +1,6 @@
+'use strict';
+const CACHE_NAME='ap-straat-pwa-v1.2.0';
+const STATIC_ASSETS=['./','/index.html','./manifest.json','./icons/icon-192x192.png','./icons/icon-512x512.png','https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(STATIC_ASSETS)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(n=>Promise.all(n.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html'))))});
